bab0b4071e88feeb136c2fb1ecc2fd8cf85c45ff
Daj Carson <Daj Carson@GAJ-INSPIRON>
2020-01-09 10:14:15 -0700

    Second Prob

--- Intro/Source1.cpp
+++ Intro/Source1.cpp
@@ -7,16 +7,25 @@ using namespace std;
 
 int main()
 {
-	double pounds;
-	cout << "Please enter a postive number for pounds";
-	cin >> pounds;
+	/*
+		double pounds;
+		cout << "Please enter a postive number for pounds";
+		cin >> pounds;
 	 
-	double kilograms = pounds * 0.454;
+		double kilograms = pounds * 0.454;
 
-	cout << kilograms << endl;
+		cout << kilograms << endl;
 
-	cout << "end of prog" << endl;
+		cout << "end of prog" << endl;
+	*/
+	double hexSide;
+	
+	cout << "Please enter in a positive number for a hexagon side" << endl;
+	cin >> hexSide;
 
+	double areaHex = ((3 * sqrt(3))/2 * pow(hexSide, 2));
+	cout << areaHex <<endl;
 
 	return 0;
+
 }
