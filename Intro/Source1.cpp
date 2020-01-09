7a35d4fdf0304f438c8c7c546481dd961a1d1e08
Daj Carson <Daj Carson@GAJ-INSPIRON>
2020-01-09 10:28:13 -0700

    Third Problem

--- Intro/Source1.cpp
+++ Intro/Source1.cpp
@@ -8,6 +8,8 @@ using namespace std;
 int main()
 {
 	/*
+		Problem 1
+
 		double pounds;
 		cout << "Please enter a postive number for pounds";
 		cin >> pounds;
@@ -17,15 +19,39 @@ int main()
 		cout << kilograms << endl;
 
 		cout << "end of prog" << endl;
-	*/
-	double hexSide;
+
+		----------------------------------------------------------------------------------------------------
+		Problem 2
+
+		double hexSide;
 	
-	cout << "Please enter in a positive number for a hexagon side" << endl;
-	cin >> hexSide;
+		cout << "Please enter in a positive number for a hexagon side" << endl;
+		cin >> hexSide;
 
-	double areaHex = ((3 * sqrt(3))/2 * pow(hexSide, 2));
-	cout << areaHex <<endl;
+		double areaHex = ((3 * sqrt(3))/2 * pow(hexSide, 2));
+		cout << areaHex <<endl;
+	
+	Problem 3
+	--------------------------------------------------------------------------------
+	*/
+	int num1;
+	int num2;
+	int larger;
 
+	cout << "Please enter in a positive number" << endl;
+	cin >> num1;
+	
+	cout << "One more time please"<< endl;
+	cin >> num2;
+
+	for (int i = 0; i < 2; i++)
+	{
+		larger = fmax(num1, num2);
+	}
+
+	cout << larger;
+	
 	return 0;
+	
 
 }
